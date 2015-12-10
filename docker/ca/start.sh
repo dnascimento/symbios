4a5abb32bf50e36cafeb4cea0fdd5fac9faeae4c
Dario Nascimento <dfrnascimento@gmail.com>
2015-12-10 11:20:06 +0000

    ben contribution

--- docker/ca/start.sh
+++ docker/ca/start.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+#nothing up my sleeve
+docker rm -fv $(docker ps -aq) && docker ps
 
-: ${SYM_PORT:=33004}
-: ${SYM_KEY_SIZE:=4098}
-: ${SYM_ORGANIZATION:=symbios}
-: ${SYM_COUNTRY:=US}
-: ${SYM_DAYS:=3650}
+docker run -d --name ca symbios/ca bash -c "symbios new-user | tee user_key && export SYM_USER_KEY=\$(cat user_key) && /start.sh && while true; do echo alive; sleep 300; done"
 
-symbios ca -port $SYM_PORT -user-key $SYM_USER_KEY -key-size $SYM_KEY_SIZE -organization $SYM_ORG -country $SYM_COUNTRY -days $SYM_DAYS &
-exec "$@"
+IP=$(docker ps | grep ca | cut -d' ' -f1 | xargs -I {} docker inspect {} | grep \"IPA | cut -d'"' -f4)
+
+for i in `seq 60`; do
+  echo -n "Checking if the CA is responding:   "
+  curl $IP:33004
+  if [ $? = 0 ]; then
+    break
+  fi
+  sleep 1
+done
+
+docker exec -t ca bash -c "symbios ca-hash --host 127.0.0.1 | tee ca_fingerprint" > fingerprint
+
+docker exec -ti ca bash -c "symbios new-token -key id_rsa | tee latest_key" > latest_key
+
+docker run -it -e "SYM_TOKEN=$(cat latest_key)" -e "SYM_CA_HASH=$(cat fingerprint)" -e "SYM_CA_HOST=$IP" symbios/container bash -c "cd /etc/secret/ && ls && bash"
